42b45824209f9b778f891deaf093efd4544dc895
emresutuna <emre.sutuna1998@gmail.com>
2019-05-27 19:00:05 +0300

    add coin

--- lib/QuestionPage.dart
+++ lib/QuestionPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class QuestionPage extends StatefulWidget {
   QuestionPage({Key key}) : super(key: key);
 
@@ -9,42 +10,62 @@ class _QuestionPageState extends State<QuestionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: new Container(
-         child: new Stack(
-           children: <Widget>[
-             new Column(
-               children: <Widget>[
-                 new Container(
-                   color: Colors.redAccent,
-                   height: 100,
-                   width: MediaQuery.of(context).size.width,
-                   child: new Column(
-                     
-                     crossAxisAlignment: CrossAxisAlignment.center,
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     children: <Widget>[
-                       Padding(
-                         padding: const EdgeInsets.only(top:30),
-                         child: new Row(
-                           children: <Widget>[
+      floatingActionButton: new FloatingActionButton(
+        onPressed: (){
 
- Padding(padding: const EdgeInsets.only(top:00),child: new Icon(Icons.monetization_on,size: 34,)),
-                         Padding(
-                           padding: const EdgeInsets.only(top:00.0),
-                           child: new Text("500",style: new TextStyle(fontSize: 24),),
-                         )
-                           ],
-                         ),
-                       ),
-                      
-                     ],
-                   ),
-                 )
-               ],
-             )
-           ],
-         ),
-       ),
+        },
+        child: new Icon(Icons.search),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: new Container(
+        child: new Stack(
+          children: <Widget>[
+            new Column(
+              children: <Widget>[
+                new Container(
+                  color: Colors.redAccent,
+                  height: 100,
+                  width: MediaQuery.of(context).size.width,
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30),
+                        child: new Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                         
+                                
+                                  
+                                     new ListTile(
+                                       dense: false,
+                                      trailing: new Row(
+                                        children: <Widget>[
+                                          new Icon(Icons.monetization_on,size: 34,),
+                                          new Text("500",style: new TextStyle(fontSize: 22),)
+                                        ],
+                                      ),
+                                      
+                                    ),
+                                  
+                                
+                              
+                           
+                            
+                            Align(alignment: Alignment.centerRight,
+                            child: new Text("data"),)
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }

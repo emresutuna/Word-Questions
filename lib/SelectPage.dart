69f766b3f3086505c8e5a621b2ac82f7eb7b447c
emresutuna <emre.sutuna1998@gmail.com>
2019-05-27 14:18:29 +0300

    modified

---
+++ lib/SelectPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SelectPage extends StatefulWidget {
+  SelectPage({Key key}) : super(key: key);
+
+  _SelectPageState createState() => _SelectPageState();
+}
+
+class _SelectPageState extends State<SelectPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
